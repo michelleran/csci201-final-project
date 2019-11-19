dc218786a31aa4207b3bb0b3a3e7978e0a537d15
rithwiksivakumar <rithwiksivakumar@Rithwiks-MacBook-Pro.local>
2019-11-18 21:10:08 -0800

    Chat and Offer classes

---
+++ ServiceRequest/ServiceRequest/Model/Chat.swift
@@ -0,0 +1,8 @@
+//
+//  Chat.swift
+//  ServiceRequest
+//
+//  Created by Rithwik Sivakumar on 11/18/19.
+//
+
+import Foundation
---
+++ ServiceRequest/ServiceRequest/Model/Offer.swift
@@ -0,0 +1,8 @@
+//
+//  Offer.swift
+//  ServiceRequest
+//
+//  Created by Rithwik Sivakumar on 11/18/19.
+//
+
+import Foundation
