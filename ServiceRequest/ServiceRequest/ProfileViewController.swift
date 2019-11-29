5ad60e10c7e7127d6570762ccff9aa226290370c
satoshidg <satoshidg3104@gmail.com>
2019-11-28 18:55:16 -0800

    Implemented parts of functions and connected em to storyboard

--- ServiceRequest/ServiceRequest/ProfileViewController.swift
+++ ServiceRequest/ServiceRequest/ProfileViewController.swift
@@ -13,7 +13,11 @@ class ProfileViewController: UIViewController {
     
     // MARK: Properties
     @IBOutlet var username: UITextView!
+    @IBOutlet var edit_or_finish: UIButton!
+    @IBOutlet var cancelBtn: UIButton!
     
+    var beforeEdit = ""
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // if the user is not logged in, then
@@ -35,38 +39,63 @@ class ProfileViewController: UIViewController {
         
     }
 
-    // TODO: implement edit name
-    @IBAction func editUsername(_ sender: UIButton) {
-        
+    // If its in editing, then finish editing
+    @IBAction func editOrFinishEditingUsername(_ sender: UIButton) {
+        if (edit_or_finish.titleLabel!.text == "Edit"){
+            username.isEditable = true
+            edit_or_finish.titleLabel!.text = "Finish"
+            beforeEdit = username.text
+            cancelBtn.isHidden = false
+            
+            // TODO: show cursur and keyboard
+        }else {
+            // Finished editing username
+            
+            username.isEditable = false
+            edit_or_finish.titleLabel!.text = "Edit"
+            cancelBtn.isHidden = true
+            
+            // TODO: update the name on backend
+            
+        }
     }
+
     
     @IBAction func logout(_ sender: UIButton) {
         Cloud.logout()
         // pull up login page
         self.presentLoginPage()
     }
+    
+    @IBAction func cancelEditing(_ sender: UIButton){
+        cancelBtn.isHidden = true
+        
+        username.text = beforeEdit
+        edit_or_finish.titleLabel!.text = "Edit"
+    }
+
 }
 
-class ProfileInEditViewController: UIViewController{
-    
-    @IBOutlet weak var username: UITextView!
-
-    override func viewDidLoad() {
-        
-    }
-    
-    @IBAction func cancelEditing(_ sender: UIButton) {
-        
-    }
-    
-    @IBAction func finishedEditing(_ sender: UIButton) {
-        // TODO: get name from UI element
-        let updated_name = ""
-        
-        // TODO: update the value on backend
-        
-        
-        // TODO: move back to the profile scene with name as parameter
-
-    }
-}
+//class ProfileInEditViewController: UIViewController{
+//
+//    @IBOutlet weak var username: UITextView!
+//
+//    override func viewDidLoad() {
+//
+//    }
+//
+//    @IBAction func cancelEditing(_ sender: UIButton) {
+//
+//    }
+//
+//    @IBAction func finishedEditing(_ sender: UIButton) {
+//        // TODO: get name from UI element
+//        let updated_name = ""
+//
+//        // TODO: update the value on backend
+//
+//
+//        // TODO: move back to the profile scene with name as parameter
+//
+//    }
+//}
